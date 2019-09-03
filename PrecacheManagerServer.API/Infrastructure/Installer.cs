7b1e10749a7b5b8ad177637b60a0550e0d08e704
Wayne Campbell <Wayne.Campbell@ebiquity.com>
2019-09-03 09:12:10 +0100

    reorganised project structure

---
+++ PrecacheManagerServer.API/Infrastructure/Installer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using PrecacheManagerServer.BLL.Repositorys;
+using PrecacheManagerServer.BLL.Services;
+using PrecacheManagerServer.DAL.Models;
+
+
+namespace PrecacheManagerServer.API.Infrastructure
+{
+    public class Installer
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<DataContext>();
+
+            services.AddTransient<IBaseRepository<PrecacheSearch>,BaseRepository<PrecacheSearch>>();
+            services.AddTransient<IBaseService<PrecacheSearch>,BaseService<PrecacheSearch>>();
+            services.AddTransient<IPrecacheSearchService,PrecacheSearchService>();
+            //services.AddTransient<IErrorHandler, ErrorHandler>();
+        }
+    }
+}
